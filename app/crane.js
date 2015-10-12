50a1c1e61c51dd4240a4d847ff95cc969ff4be18
Alexey Shcherbakov <alexeykolpino@gmail.com>
2015-10-12 20:58:05 +0300

    удалил лишнее в crane.js

--- app/crane.js
+++ app/crane.js
@@ -63,52 +63,6 @@ var AXIS =
         m_app.enable_controls();
         m_app.enable_camera_controls();
     }
-    // Ищем кнопки, ожидлаем действие
-    function waiting() {
-        document.getElementById('obj1UnitR18').addEventListener("click",function(){rotateUnit("obj1Unit",18)});
-        document.getElementById('obj1UnitL18').addEventListener("click",function(){rotateUnit("obj1Unit",-18)});
-
-        document.getElementById('obj2UnitR18').addEventListener("click",function(){rotateUnit("obj2Unit",18)});
-        document.getElementById('obj2UnitL18').addEventListener("click",function(){rotateUnit("obj2Unit",-18)});
-
-        document.getElementById('obj4UnitR18').addEventListener("click",function(){rotateUnit("obj4Unit",18)});
-        document.getElementById('obj4UnitL18').addEventListener("click",function(){rotateUnit("obj4Unit",-18)});
-        //but2.addEventListener("click",rotateUserObj());
-    }
-    function rotateUnit(o,d){
-        // Далее по образу и подобию примера на странице 70 руководства
-        // находим объект
-        var rotateUnit = m_scenes.get_object_by_name(o);
-        // переводим из градусов в радианы
-        function degToRad(d){return d*(Math.PI/180)}
-        // объявим оси
-        var Y = new Float32Array([0, 1, 0]);
-        var X = new Float32Array([1, 0, 0]);
-        var Z = new Float32Array([0, 0, 1]);
-        var AXIS;
-        // мы знаем, какие у каждого объекта степени свободы, поэтому на основании имени решим по какой оси их вращать
-        switch (o){
-            case "obj1Unit":
-                AXIS = Z;
-                break;
-            case "obj2Unit":
-                AXIS = Z;
-                break;
-            case  "obj4Unit":
-                AXIS = Y;
-                break;
-            default :
-                console.log('Проверьте названия объектов, что передаются в функцию rotateUnit(~62 строка crane.js)');
-        }
-
-        var quat_tmp = new Float32Array(4);
-        var quat_tmp2 = new Float32Array(4);
-
-        m_quat.setAxisAngle(AXIS,-degToRad(d),quat_tmp);
-        m_trans.get_rotation(rotateUnit, quat_tmp2);
-        m_quat.multiply(quat_tmp, quat_tmp2, quat_tmp);
-        m_trans.set_rotation_v(rotateUnit, quat_tmp);
-    }
 
 
 });
