1e7148ef7c9316921c50391aed30e266f9a2e9d8
Alexey Shcherbakov <alexeykolpino@gmail.com>
2015-10-20 10:27:11 +0300

    class RotationArea

--- app/rotationarea.js
+++ app/rotationarea.js
@@ -1,3 +1,18 @@
-/**
- * Created by alexey on 18.10.15.
- */
+// 2. Конструктор Hand
+function RotationArea(name,modules,AXIS){
+    Particles.apply(this,arguments);
+}
+// 2.1 Наследование
+RotationArea.prototype = Object.create(Particles.prototype);
+RotationArea.prototype.constructor = RotationArea;
+
+// 2.2 Методы
+
+// Вызов родительского метода из дочернего
+RotationArea.prototype.eventListener = function (eventName,func) {
+    RotationArea.prototype.eventListener.apply(this,arguments);
+};
+
+RotationArea.prototype.rotate = function(deg, axis) {
+    RotationArea.prototype.rotate.apply(this,arguments);
+};
