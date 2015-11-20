2cb5035e17847df15e340158e819de1809a8547e
Alexey Shcherbakov <alexeykolpino@gmail.com>
2015-11-20 20:45:41 +0300

    Вычисления с векторами вынесены в отдельный класс vector.js с методами Vector.vectorFromCoord и Vector.angleBeetweenTwoVectors

--- app/fabrik.js
+++ app/fabrik.js
@@ -3,37 +3,6 @@
  */
 function FABRIK(){
 }
-// Функция вычисления вектора (его координат)
-
-FABRIK.vectorFromCoord = function(coord1, coord2) {
-    return coord1.map(function(item,i){return coord2[i]-item})
-};
-
-// Функция для вычисления угла между 2 векторами
-FABRIK.angleBetweenTwoVectors = function(vector1, vector2) {
-    // скалярное произведение векторов
-    var scalMultVectors = vector1.reduce(function(sum, current, i) {
-        return sum + (current * vector2[i])
-    }, 0);
-    // модуль вектора равен квадратному корню из суммы квадратов его координат
-    var moduleVector = function(v) {
-        // Находим квадраты слагаемых
-        var step1 = v.map(function(currentValue) {
-            return Math.pow(currentValue, 2)
-        });
-        // Складываем их
-        var step2 = step1.reduce(function(sum, current) {
-            return sum + current
-        });
-        // Вычисляем квадратный корень
-        return Math.sqrt(step2, 2)
-    };
-    // Вычисляем косинус угла между векторами
-    var cosA = scalMultVectors / (moduleVector(vector1) * moduleVector(vector2));
-    console.log("cos(" + cosA + ")");
-    return Math.acos(cosA);
-
-};
 
 FABRIK.prototype.algorithm = function(arrayOfInitialPositions, TargetPoint, tol){
     // узел pn = [[x,y,z]]
