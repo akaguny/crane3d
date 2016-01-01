c1fbb52b26d54b971330ea931d285c5c18c65aec
Alexey Shcherbakov <alexeykolpino@gmail.com>
2016-01-01 14:55:48 +0300

    Скалярное произведение вынесено в отдельный метод. Дописана нормализация векторов. Todo: необходимо определять сонаправленность векторов,т.к. есть ошибка со знаком угла поворота

--- app/vector.js
+++ app/vector.js
@@ -9,7 +9,7 @@ Vector.vectorFromCoord = function(coord1, coord2){
     return vector
 };
 
-// модуль вектора равен квадратному корню из суммы квадратов его координат
+// Модуль вектора равен квадратному корню из суммы квадратов его координат
 Vector.module = function (vector) {
     // Находим квадраты слагаемых
     var step1 = vector.map(function(currentValue) {
@@ -24,24 +24,35 @@ Vector.module = function (vector) {
     return length;
 };
 
-// Функция для вычисления угла между 2 векторами
-Vector.angleBetweenTwoVectors = function(vector1, vector2) {
-    // скалярное произведение векторов
-    var vector1 = vector1, vector2 = vector2;
-    var scalMultVectors = vector1.reduce(function(sum, current, i) {
+// Скалярное произведение векторов
+Vector.scalMultVectors = function (vector1,vector2) {
+    var result = vector1.reduce(function(sum, current, i) {
         return sum + (current * vector2[i])
     }, 0);
+    return result
+
+};
+
+// Функция для вычисления угла между 2 векторами0
+Vector.angleBetweenTwoVectors = function(vector1, vector2) {
+    // скалярное произведение векторов
     // Вычисляем косинус угла между векторами
-    var cosA = scalMultVectors / (Vector.module(vector1) * Vector.module(vector2));
+    var cosA = Vector.scalMultVectors(vector1, vector2) / (Vector.module(vector1) * Vector.module(vector2));
     var rad = Math.acos(cosA);
     return rad
 
 };
 
+// Перевод из радиан в градусы
 Vector.radToAngle = function(rad){
     return rad * (180/Math.PI)
 };
 
+// Нормализация вектора
 Vector.normalize = function (vector) {
+    var invLength = 1/Vector.module(vector);
+    var normalizeVector = vector.map(function (item) {
+        return item * invLength
+    });
     return normalizeVector
 };
