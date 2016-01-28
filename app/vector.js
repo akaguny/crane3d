50a5bbaf4bd9a2193793bc7332011f6566a12906
Alexey Shcherbakov <alexeykolpino@gmail.com>
2016-01-28 23:39:38 +0300

    Убрано лишнее, сделано вычисления угла для двумерного пространства, судя по результату вполне адекватно

--- app/vector.js
+++ app/vector.js
@@ -9,7 +9,7 @@ Vector.vectorFromCoord = function(coord1, coord2){
     return vector
 };
 
-// модуль вектора равен квадратному корню из суммы квадратов его координат
+// модуль(length) вектора равен квадратному корню из суммы квадратов его координат
 Vector.module = function (vector) {
     // Находим квадраты слагаемых
     var step1 = vector.map(function(currentValue) {
@@ -24,18 +24,31 @@ Vector.module = function (vector) {
     return length;
 };
 
+// Векторное произведение векторов
+//Vector.vecMultVectors = function(a, b) {
+//    var c =[];
+//    c[0] = a[1] * b[2] - a[2] * b[1]; //x
+//    c[1] = a[2] * b[0] - a[0] * b[2]; //y
+//    c[2] = a[0] * b[1] - a[1] * b[0]; //z
+//    console.log(c);
+//    return c;
+//};
+// Скалярное произведение векторов
+Vector.scalMultVectors = function (vector1,vector2) {
+    return vector1.reduce(function(sum, current, i) {
+        return sum + (current * vector2[i])
+    }, 0);
+};
+
 // Функция для вычисления угла между 2 векторами
 Vector.angleBetweenTwoVectors = function(vector1, vector2) {
     // скалярное произведение векторов
     var vector1 = vector1, vector2 = vector2;
-    var scalMultVectors = vector1.reduce(function(sum, current, i) {
-        return sum + (current * vector2[i])
-    }, 0);
     // Вычисляем косинус угла между векторами
-    var cosA = scalMultVectors / (Vector.module(vector1) * Vector.module(vector2));
-    var rad = Math.acos(cosA);
-    return rad
-
+    //var cosA = Vector.scalMultVectors / (Vector.module(vector1) * Vector.module(vector2));
+    //var rad = Math.acos(cosA);
+    //return Math.atan2(Math.pow(Vector.module(Vector.vecMultVectors(vector1,vector2)),2),Vector.scalMultVectors(vector1,vector2))
+    return Math.atan2(vector1[0]*vector2[1]-vector2[0]*vector1[1],vector1[0]*vector2[0]+vector1[1]*vector2[1])
 };
 
 Vector.radToAngle = function(rad){
