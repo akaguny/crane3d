577ff5b86731a9d446d25fec84378519f7f30de9
Alexey Shcherbakov <alexeykolpino@gmail.com>
2016-02-22 14:19:23 +0300

    создал класс для работы с gui, пока поддерживается только работа с input

---
+++ app/craneGUI.js
@@ -0,0 +1,18 @@
+"use strict";
+function CraneGUI(id) {
+    this.htmlElement = document.getElementById(id);
+    this.type = this.htmlElement.tagName;
+    //if (this.type === "INPUT")
+}
+
+CraneGUI.prototype.set_value = function (value) {
+    //if (this.type === "INPUT")
+        this.htmlElement.value = value;
+};
+CraneGUI.prototype.get_value = function () {
+    //if (this.type === "INPUT")
+    return +this.htmlElement.value
+};
+CraneGUI.prototype.addEventListener = function (event,func) {
+    this.htmlElement.addEventListener(event,func);
+};
